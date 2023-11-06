d1c6db5c51659662d2aae98d30e908e0e329f24a
hueam <72968569+hueam@users.noreply.github.com>
2023-11-06 13:01:34 +0900

    trailDelete

--- RhythmFPS/Assets/01.Scripts/Player/Weapon/SimpleWeapon.cs
+++ RhythmFPS/Assets/01.Scripts/Player/Weapon/SimpleWeapon.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SimpleWeapon : Weapon
 {
-    public BulletTrail _trailPrefab;
     public Transform _firePos;
 
     private void Awake()
@@ -30,18 +29,12 @@ public class SimpleWeapon : Weapon
         print("¸ÂÀ½!");
         _currentBullet--;
         _animator.SetRigTriggerFIre(true);
-        BulletTrail trail = Instantiate(_trailPrefab);
         if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out RaycastHit hit, 50f, _whatIsEnemy))
         {
             if (hit.transform.TryGetComponent<IDamageable>(out IDamageable damageable))
             {
                 damageable.TakeDamage(5);
             }
-            trail.DrawTrail(_firePos.position, hit.point, 0.1f);
-        }
-        else
-        {
-            trail.DrawTrail(_firePos.position, _cam.transform.forward * 500f, 0.1f);
         }
         return;
     }
