60ad90667f40dfedf8c5a50b613325c5d52aae1a
Gusdnd01 <cestop42@naver.com>
2023-11-06 13:00:34 +0900

    맵 구성중

--- RhythmFPS/Assets/01.Scripts/FX/EffectPlayer.cs
+++ RhythmFPS/Assets/01.Scripts/FX/EffectPlayer.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using MoreMountains;
+using MoreMountains.Feedbacks;
 
 public class EffectPlayer : MonoBehaviour
 {
     public List<ParticleSystem> fx = new List<ParticleSystem>();
+    private MMF_Player player;
 
     private void Update()
     {
@@ -15,8 +18,14 @@ public class EffectPlayer : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        player = GetComponent<MMF_Player>();
+    }
+
     public void EffectPlay()
     {
         fx.ForEach(e => e.Play());
+        player.PlayFeedbacks();
     }
 }
