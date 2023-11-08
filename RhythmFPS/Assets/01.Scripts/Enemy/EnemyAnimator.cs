ae960d1c431c0d49f020ecff42efc808af3f9519
botw0303 <zeldabotw0303@gmail.com>
2023-11-09 00:53:18 +0900

    stage 1 boss (unfinished) 11/09

--- RhythmFPS/Assets/01.Scripts/Enemy/EnemyAnimator.cs
+++ RhythmFPS/Assets/01.Scripts/Enemy/EnemyAnimator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyAnimator : MonoBehaviour
 {
+    private readonly int _isDeadHash = Animator.StringToHash("is_dead");
+    private readonly int _isAttackHash = Animator.StringToHash("attack");
+    private readonly int _isMoveHash = Animator.StringToHash("is_move");
+
     private Animator _animator;
     public Animator Animator => _animator;
 
@@ -12,4 +16,10 @@ public class EnemyAnimator : MonoBehaviour
         _animator.speed = value ? 0 : 1; //true일 때 0으로 만들어서 정지, 
         //아닐때 1로 만들어서 다시 재생
     }
+
+    public void SetDead()
+    {
+        _animator.SetBool(_isDeadHash, true);
+    }
+
 }
