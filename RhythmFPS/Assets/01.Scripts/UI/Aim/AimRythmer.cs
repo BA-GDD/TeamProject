4c21dfa95eeb7c71ae6b61aa9faad8a5e7a27751
VsPot07 <cocoacake84@gmail.com>
2023-11-20 22:19:56 +0900

    1120 add feedback

--- RhythmFPS/Assets/01.Scripts/UI/Aim/AimRythmer.cs
+++ RhythmFPS/Assets/01.Scripts/UI/Aim/AimRythmer.cs
@@ -24,4 +24,18 @@ public class AimRythmer : MonoBehaviour
         Debug.Log("Reset");
         _aimMaster.color = _normalColor;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            RhythmReaction();
+    }
+
+    public void RhythmReaction()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append( transform.DOScaleX(1.85f, 0.1f));
+        seq.Append(transform.DOScaleX(2f, 0.1f));
+    }
 }
