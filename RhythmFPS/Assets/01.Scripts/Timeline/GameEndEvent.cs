ac8e32f96390c225594ef3bea748d12fe9852007
Gusdnd01 <cestop42@naver.com>
2023-11-20 17:57:24 +0900

    보스 죽는 연출

---
+++ RhythmFPS/Assets/01.Scripts/Timeline/GameEndEvent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameEndEvent : MonoBehaviour
+{
+    [SerializeField]
+    private PlayableDirector _director;
+
+
+    public void DirectorEndEvent(PlayableDirector director)
+    {
+        //여기 추가하면 된다.
+        print("게임 끝");
+    }
+    public void GameEnd()
+    {
+        _director.stopped += DirectorEndEvent;
+        _director.Play();
+    }
+}
