5dbba0ce0747c49f5cdc2e5a2ff0676ff7620de2
botw0303 <zeldabotw0303@gmail.com>
2023-11-21 17:36:39 +0900

    merge

---
+++ RhythmFPS/Assets/01.Scripts/Timeline/GameEndEvent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameEndEvent : MonoBehaviour
+{
+    [SerializeField]
+    private PlayableDirector _director;
+
+
+    public void DirectorEndEvent(PlayableDirector director)
+    {
+        //여기 추가하면 된다.
+
+        GameManager.instance.gameObject.BroadcastMessage("PushObject");
+        GameManager.instance.GameClear();
+        print("게임 끝");
+    }
+    public void GameEnd()
+    {
+        GameManager.instance.PlayerTransform.GetComponent<PlayerHealth>().isCanHit = false;
+        _director.stopped += DirectorEndEvent;
+        _director.Play();
+    }
+}
